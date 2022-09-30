9674cca46dbdb23938d68b9deabfe844ca9e6bf7
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-09-30 14:55:47 -0300

    Design System em React - Configurando pacote do React

--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from '@ignite-ui/tokens'
-
-console.log(colors)
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,5 @@
+import { colors } from '@ignite-ui/tokens'
+
+export function App() {
+  return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
+}
