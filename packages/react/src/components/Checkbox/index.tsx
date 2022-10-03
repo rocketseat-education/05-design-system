d88ef430012f086648f56bd613b1a93931799846
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-10-03 15:32:01 -0300

    Componentes do Design System - Componente: Checkbox

---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,16 @@
+import { Check } from 'phosphor-react'
+import { ComponentProps } from 'react'
+import { CheckboxContainer, CheckboxIndicator } from './styles'
+
+export interface CheckboxProps
+  extends ComponentProps<typeof CheckboxContainer> {}
+
+export function Checkbox(props: CheckboxProps) {
+  return (
+    <CheckboxContainer {...props}>
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  )
+}
