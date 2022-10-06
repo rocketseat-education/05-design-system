0ecf1544018411388cf2f8033f161d03a94ea7e8
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-10-06 11:43:42 -0300

    Publicação e manutenção - Publicando Storybook

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -15,5 +15,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/05-design-system/'
+    }
+
+    return config
   }
 }
