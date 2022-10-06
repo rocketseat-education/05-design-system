135bd2bd8c1b7771467d3b7aae62cbddbf296e35
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-10-06 11:13:13 -0300

    Melhorias no Storybook - Log de ações no componente

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -8,6 +8,9 @@ export default {
   args: {
     children: 'Send',
   },
+  argTypes: {
+    onClick: { action: 'clicked' },
+  },
 } as Meta<ButtonProps>
 
 export const Primary: StoryObj<ButtonProps> = {}
